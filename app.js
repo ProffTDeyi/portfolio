348234837180a921b6ad060a20790cd5b16803d3
Deyi <profesordeyi@gmail.com>
2021-05-20 13:57:01 +0200

    ready to push for the first time

--- app.js
+++ app.js
@@ -10,3 +10,23 @@
     closeMenu.addEventListener("click", () => {
         navLinks.style.top = "-210px";
     });
+
+
+        // code for the social icons
+
+    const social_Icons = document.querySelectorAll(".social-media-icon img");
+    for (let icon of Array.from(social_Icons).slice(1)){
+        icon.style.borderRadius = "50%";
+    }
+
+    
+    // code for calling icons
+
+    let callNum = document.querySelectorAll(".number");
+    document.querySelector(".call_me")
+    .addEventListener("click", e => {
+        for (let num of Array.from(callNum)){
+        alert(`You Can Quickly Call Me On These Number(s): ${num.textContent}`);
+        e.preventDefault();
+    }
+    });
